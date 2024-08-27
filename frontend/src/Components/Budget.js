016ac6d85407a1bec028c4f73f855caa94c6273a
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-08-27 12:13:20 -0400

    fixed category menu layout

--- frontend/src/Components/Budget.js
+++ frontend/src/Components/Budget.js
@@ -31,13 +31,12 @@ export default function Budget(){
     const [ anchorEl, setAnchorEl ] = useState(null);
     const [ budgetCreated, setBudgetCreated ] = useState(false);
     const [ categories, setCategories ] = useState([]);
-    const [ category, setCategory ] = useState("");
+    const [ menuItem, setMenuItem ] = useState("");
     const categoryList = [];
     let open = Boolean(anchorEl);
 
     const handleMenuOpen = (event) => {
-        console.log(event.currentTarget);
-        setAnchorEl(event.currentTarget.innerHTML);
+        setAnchorEl(event.currentTarget.parentElement);
     };
 
     const handleMenuClose = () => {
@@ -45,12 +44,12 @@ export default function Budget(){
     }
 
     const menuClick = (chosenCategory) => {
-        setCategory(chosenCategory);
+        setMenuItem(chosenCategory);
         setAnchorEl(null);
     }
 
     const addCategory = (event)=> {
-        let newCat = new Category(category, event.currentTarget.form.amount.value);
+        let newCat = new Category(menuItem, event.currentTarget.form.amount.value);
         let newCats = categories;
         // for (let i = 0; i < categories.length; i++) {
         //     // note: we are adding a key prop here to allow react to uniquely identify each
@@ -63,10 +62,11 @@ export default function Budget(){
         // }
         newCats.push(newCat);
         setCategories(newCats);
+        setAnchorEl(null);
     }
 
     const createBudget = (event) => {
-        const newBudg = new BudgetObj(event.currentTarget.form.name.value, event.currentTarget.form.amonut.value, categories);
+        const newBudg = new BudgetObj(event.currentTarget.form.name.value, event.currentTarget.form.amount.value, categories);
         fetch('http://localhost:8080/createBudget',
         {
             headers: {
@@ -81,7 +81,8 @@ export default function Budget(){
         .catch(error => console.error(error));
         setBudgetCreated(true);
     }
-    
+    //useEffect(()=>{},[categories]) // this will solve your issue 
+
     return(
         <div style={{width: '100vw'}}>
             <h1>Create Your Budget</h1>
@@ -92,21 +93,23 @@ export default function Budget(){
                             <Input name="budget-name" sx={{width: 200}} placeholder="Budget Name" required></Input>
                             <Input name="total" sx={{width: 200}} placeholder="Total" required></Input>
                         </div>
-                        <div style={{backgroundColor:'black'}}>{categories.map((category,i) => <div key={i}>{category.name}</div>)}</div>
+                        <div style={{width:'100%', display:'flex', flexDirection:'column',columnGap:'5%', justifyContent:'center'}}>{categories.map((category,i) => <h3 key={category.name+i}>{category.name + ": $" + category.amount}</h3>)}</div>
                         <Button onClick={createBudget} variant = "outlined" style={{color:'white', marginTop:"2%"}}>Create Budget</Button>
                     </form>
                     <div style={{width: '100%', marginTop:'1%'}}>
-                        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose} anchorOrigin={{vertical:'bottom', horizontal:'left'}}>   
+                        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose} anchorOrigin={{vertical:'bottom', horizontal:'center'}}>   
                             <MenuItem onClick={()=>menuClick(CATEGORIES.GROCERY)}>Grocery</MenuItem>
                             <MenuItem onClick={()=>menuClick(CATEGORIES.DISCR)}>Discretionary</MenuItem>
                             <MenuItem onClick={()=>menuClick(CATEGORIES.SAVINGS)}>Savings</MenuItem>
                             <MenuItem onClick={()=>menuClick(CATEGORIES.PHONE)}>Phone</MenuItem>
                             <MenuItem onClick={()=>menuClick(CATEGORIES.INTERNET)}>Internet</MenuItem>
                         </Menu>
-                        <div style={{display: 'flex', alignItems:'center', justifyContent:'center'}}>
-                            <h3>Category: {category}</h3>
-                            <ArrowDropDownIcon style={{display: anchorEl === null ? 'block': 'none'}}onClick={handleMenuOpen}></ArrowDropDownIcon>
-                            <ArrowDropUpIcon style={{display:anchorEl === null ? 'none': 'block'}}onClick={()=>setAnchorEl(null)}></ArrowDropUpIcon>
+                        <div style={{display: 'flex', alignItems:'center', justifyContent:'center', width:'100%'}}>
+                            <div style={{display: 'flex', alignItems:'center'}}>
+                                <h3>Category: {menuItem}</h3>
+                                <ArrowDropDownIcon style={{display: anchorEl === null ? 'block': 'none'}}onClick={handleMenuOpen}></ArrowDropDownIcon>
+                                <ArrowDropUpIcon style={{display:anchorEl === null ? 'none': 'block'}}onClick={handleMenuClose}></ArrowDropUpIcon>
+                            </div>
                         </div>
                     </div>
                     <form style={{width:'100%', display:'flex', columnGap:'5%', justifyContent:'center',alignItems:'center'}}>
