2ebcc065113c23a04af92db9876a0b53bd9f1bb2
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-11-01 18:00:42 -0400

    pushing changes before changing repository implementation to accomodate users

--- backend/src/main/java/com/budgeter/server/Entities/User.java
+++ backend/src/main/java/com/budgeter/server/Entities/User.java
@@ -1,4 +1,57 @@
 package com.budgeter.server.Entities;
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="Users")
 public class User {
+
+    private @Id @GeneratedValue Long id;
+    private String username;
+    private String password;
+    List<Budget> budgets;
+
+    public User(){}
+    public User(Long id, String username, String password, List<Budget> budgets) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.budgets = budgets;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Budget> getBudgets() {
+        return budgets;
+    }
+
+    public void setBudgets(List<Budget> budgets) {
+        this.budgets = budgets;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
