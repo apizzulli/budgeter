caf135ea08714d6e6db004dbaba1a03c982e7a99
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-11-06 15:46:45 -0500

    budget view fully functional

--- frontend/src/Components/ViewBudgets.js
+++ frontend/src/Components/ViewBudgets.js
@@ -1,6 +1,7 @@
 import { ListItem } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useLocation } from 'react-router-dom';
+import Button from '@mui/joy/Button';
 
 export default function ViewBudgets(props){
 
@@ -11,23 +12,36 @@ export default function ViewBudgets(props){
     useEffect(()=>{
         fetch(`http://localhost:8080/getBudgets/${location.state.userId}`)
         .then(response => response.json())
-        .then(data => setBudgets(data.data.budgets))
+        .then(data => setBudgets(data))
         .catch(error => console.error(error));
     });
     function click(){
-        console.log(location.state);
+        console.log(budgets);
     }
 
-    // if(!userId){
-    //     return(<div>No user id!</div>)
-    // }
+    const budgetView = (budget) => {
+        let categories = budget.categories;
+        return(
+            <div>
+                <h1>{budget.name + ": $" + budget.total}</h1>
+                <Button></Button>
+                <div>
+                    Categories:
+                    {Object.keys(budget.categories).map((name) => <div key={name} style={{width:'100%', display:'flex', flexDirection:'column',columnGap:'5%', justifyContent:'center'}}>{name + ": $" + categories[name]}</div>)}
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div>
             <button onClick={click}>click</button>
             <h1>Welcome to budgets view</h1>
             <h2>Your existing budgets:</h2>
-            {budgets.map((budget,i) => <div key={i} style={{width:'100%', display:'flex', flexDirection:'column',columnGap:'5%', justifyContent:'center'}}>{budget.name + ": $" + budget.total}</div>)}
+            {budgets.map((budget,i) => 
+                <div key={i} style={{width:'100%', display:'flex', flexDirection:'column',columnGap:'5%', justifyContent:'center'}}>
+                    {budgetView(budget)}
+                </div>)}
         </div>
     )
 }
